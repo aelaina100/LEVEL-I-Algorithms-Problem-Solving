e083f0e7b3e0ca71f040f95d93565185cd0238ec
aelaina100 <aelaina100@gmail.com>
2026-03-10 00:26:36 -0400

    Adding more solutions

---
+++ Problem #2/Problem #2/Problem #2.cpp
@@ -0,0 +1,61 @@
+﻿
+
+#include <iostream>
+#include <string>;
+using namespace std;
+
+
+/*
+ Problem #2
+
+ Write a program to ask the user to enter his/her name and
+ print it on screen.
+
+*/
+
+
+
+// Note: This is NOT the final solution. 
+// As, soon, we're going to include input validation.
+// the point is to focus on logic now and then input validation will be added.
+
+
+string ReadName()
+{
+	string name; // What is returned.
+    
+	cout << "Enter your name: ";
+	getline(cin, name); // for string inputs ALWAYS + ONLY use this line, so that a string with space(s) is take as one whole value. #include<string>  library is need for this.
+	                    // ONLY use:  cin >> x for integers.
+	return name;
+	 // The variable 'name' was initialized and then returned.
+	 // The calling function, ReadNumber(), in main(), evaluates to produce a value of type int (which initializes an object in the calling function),
+     // the lifetime of the local variable 'name' inside this function ends when the program exists the curly brackets of this function.
+	 // 🔥🔥🔥 correct !
+}                      
+// The note below, from now on, will NOT be included in the next examples ⇒
+// (it will always apply, but will not be pointed out again): ⇒
+// 
+// When constructing a procedure/function, and its params, do so IN ACCORDANCE WITH the called function/ procedure in main().
+
+void PrintName(const string &name)     // passing by constant ref. the parameter 'name' could be any other name.  
+{                                     // when constructing the parameter argument, do so IN ACCORDANCE WITH the called function.
+	cout << "Your name is: " << name << endl;
+}   
+int main()
+{
+
+	PrintName(ReadName());   // ReadName() has to be a function, as a procedure returns null. Null can not be an argument of any function or procedure.
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
---
+++ problem #9/problem #9/problem #9.cpp
@@ -0,0 +1,258 @@
+// problem #9.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
+
+#include <iostream>
+using namespace std;
+
+/*
+Problem #9:
+
+ Write a program to ask the user to enter:
+ - Number1, Number2, Number3
+
+ Then print the Sum of entered numbers.
+
+ Example Inputs:
+ 10
+ 20
+ 30
+
+ Output:
+ 60
+
+*/
+
+
+
+
+/* Solution:
+
+   Only having:
+
+   #include <iostream>
+   using namespace std;
+
+   while using the array fixed-length data structure,
+   means that an array can NEVER be returned by a function.
+
+*/
+const int NumberOfElements = 3; // Instead of hardcoding it in the fixed-length data structure array. // later I discovered that I ought to render it as gloal variable.
+
+void ReadNumbers(float numbers[NumberOfElements]) // arrays are ALWAYS passed by reference & Including the & sign, renders an exception.
+{
+    for (int i=0; i < NumberOfElements; i++)
+    {
+        cout << "Enter Number " << i + 1 << " : ";
+        cin >> numbers[i];
+        cout << endl;
+    }
+}
+
+float CalculateSumOfArrayElements(float numbers[NumberOfElements]) // arrays are ALWAYS passed by reference & Including the & sign, renders an exception.
+{
+    float sum = 0;
+    for (int i = 0; i < NumberOfElements; i++)
+    {
+        sum += numbers[i];  
+    }
+
+    return sum;
+}
+
+void PrintSumOfArrayElements(const float &sum) // arrays are ALWAYS passed by reference & Including the & sign, renders an exception.
+{
+    cout << "Their sum is: " << sum << endl; // here I will NOT be able to reference the 3 numbers (& no need for it really!) 
+}                                              // because of the current nested functions line construction in main()
+ 
+int main()
+{
+    // a function can NOT return an array that is a fixed-length data structure. And given that only    #include <iostream> library    is utilized.
+    float numbers[NumberOfElements];
+    // initializing the array variable via reading user-input:
+
+
+    ReadNumbers(numbers);   // passing the array variable as an argument. It's passed along with its memory address. 
+                            // hence, it's always passed by reference in the implementing function.
+//                         //  Including & in the implementing function will render an exception.
+   // CRUCIAL: Passing the argument: numbers[3] means you are passing the element in the 4th index which does NOT exist.
+                           
+    // The array variable 'numbers' is now fully initialized.
+   
+
+    PrintSumOfArrayElements(CalculateSumOfArrayElements(numbers));  // Where the 'numbers' array argument is passed along its address in memory. 
+                                                                    // Hence, the implementing function will pass params by ref. without the & sign.
+                                                                    // 
+    // I chose this line of nested functions becasue there is no need to reference the 
+    // original 3 numbers that the user just entered, when I want to print the sum onto the screen.
+    return 0;
+}
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Now, what if I want to reference the original 3 numbers the user just entered when I want to display the sum ?
+
+/* For example:
+
+ Write a program to ask the user to enter:
+ - Number1, Number2, Number3
+
+ Then print the Sum of entered numbers.
+
+ Example Inputs:
+ 10
+ 20
+ 30
+
+ Output:
+ The three numbers entered that are 10, 20, & 30 has a sum of 60  👈👈 If this is what I want to display (referencing the original entered 3 numbers)
+
+
+*/
+/* Solution:
+
+   Only having:
+
+   #include <iostream>
+   using namespace std;
+
+   while using the array fixed-length data structure,
+   means that an array can NEVER be returned by a function.
+
+
+   NOTE: FOR THE SOLUTION BELOW YOU MAY NEED BETTER NAMINTG FOR FUNCTIONS / PROCEDURES AS THIS WAS INDEED THE FIRST ATTEMPT EVER IN SOLVING THIS PROBLEM ( Plus I may need 
+   some editing for the comment just before I make a final release for this repository !!)
+*/
+const int NumberOfElements = 3; // as we progressed in this program, this line has proven the need to render it global.
+
+void ReadNumbers(float numbers[NumberOfElements]) // arrays are ALWAYS passed by reference. 
+{
+    for (int i = 0; i < NumberOfElements; i++)
+    {
+        cout << "Enter Number " << i + 1 << " : ";
+        cin >> numbers[i];
+        cout << endl;
+    }
+}
+
+float SumOfNumbers(float numbers[NumberOfElements])
+{
+    float sum = 0;
+    for (int i = 0; i < NumberOfElements;  i++)
+    {
+        sum += numbers[i];
+    }
+    return sum;
+}
+
+void PrintSum(float numbers[NumberOfElements]) // ALWAYS passed by ref. Including the & symbol will render an exception.
+{
+    cout << "The entered numbers: "; 
+    for (int i = 0; i < NumberOfElements; i++)
+    {
+        cout << numbers[i] << "  "; 
+    }
+    cout << endl;
+    cout << "Have the sum of " << SumOfNumbers(numbers) << endl;
+}
+
+int main()
+{
+    float numbers[NumberOfElements];
+    // initializing the array variable via reading user-input:
+
+    ReadNumbers(numbers);// passing the array variable as an argument. It's passed along with its memory address. hence, it is always passed by reference in the implementing
+                         // function. Including & will render an exception.
+     // The array variable 'numbers' is now fully initialized.
+
+    PrintSum(numbers);    
+
+
+   // PrintSum(CalculateSum(numbers)); // also valid. BUT- the printing procedure will not have access to the three numbers entered by the user.
+                                           // Hence, referencing them in the printing procedure will require adding additional lines of code in main = unoptimized approach. 
+    return 0;
+}
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/* Primitive solutions 3 variables instead of an array:
+ 
+*/
+
+
+void ReadNumbers(float& Num1, float& Num2, float& Num3) // Passing by reference. So that the variables declared in main are initialized so that we continue
+                                                        // writing our code in main(). Otherwise, we have to continue coding inside this procedure which
+                                                        // violates the SRP.
+{
+    cout << "Enter First Number: ";
+    cin >> Num1;
+    cout << endl;
+
+    cout << "Enter Second Number: ";
+    cin >> Num2;
+    cout << endl;
+
+    cout << "Enter Third Number: ";
+    cin >> Num3;
+    cout << endl;
+}
+
+float CalculateSum(float Num1, float Num2, float Num3) // passing by value.
+{
+    return (Num1 + Num2 + Num3);
+}
+
+void PrintResult(const float& Num1, const float& Num2, const float& Num3)
+{
+    cout << "The sum of the numbers: " << Num1 << " & " << Num2 << " & " << Num3 << " = " << CalculateSum(Num1, Num2, Num3) << endl;
+}
+
+int main()
+{
+    // Three variables are to be declared and initialized via reading user input
+    // where the implementing function passess params by reference.
+    // Otherwise, How would one be able to continue writing the program inside main() ?
+    // where they'll have to continue coding inside that procedure which is ridiculous and violates the SRP.
+    // 
+    // PLUS- This can only be done utilizing a procedure that will have 3 cin statements. 
+    // A function can only return ONE value.
+    float Num1, Num2, Num3;
+    // Initializing these variables via reading user input:
+
+    ReadNumbers(Num1, Num2, Num3);
+    // the variables Num1, Num2, & Num3 are now initialized.
+
+    // Now:
+    /*
+
+    PrintResult(CalculateSum(Num1, Num2, Num3)); // works but the printing proced. won't have reference to Num1, Num2, & Num3 (Ex: the sum of Num1, Numb2,.. is =) wont be possible.
+    //unless you state:
+    PrintResult(CalculateSum(Num1, Num2, Num3),  Num1, Num2, Num3); // works ! BUT- don't you think it looks hard to read?
+
+    */
+
+    // so instead, try:
+    PrintResult(Num1, Num2, Num3);
+
+    return 0;
+
+
+    /* In this SPECIFIC example, any of the following is NOT going to work:
+
+     PrintSum(CalculateSum(ReadNumbers()));
+     PrintSum(ReadNumber());
+
+     because, again, in this SPECIFIC example:
+     
+     ReadNumbers() can only be a procedure that initializes 3 numbers.
+
+     If it was to initialize one number only
+     then that one number is to be returned using a function instead.
+
+    */
+}
+   
+
+    
+
